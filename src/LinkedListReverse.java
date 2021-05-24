8a5b3a756719cac151e39dc88fdc2fd520fcba59
iwantdrinktea <51637086+iwantdrinktea@users.noreply.github.com>
2021-05-24 16:46:08 +0800

    Update LinkedListReverse.java

--- src/LinkedListReverse.java
+++ src/LinkedListReverse.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//迭代
 public class LinkedListReverse {
     public static void main(String[] args) {
         LinkedList node5 = new LinkedList(5,null);
